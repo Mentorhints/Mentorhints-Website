a7893076078d0a2dfee7c45bf099ecbfbaddfb7b
Jeyapriya843 <131690155+Jeyapriya843@users.noreply.github.com>
2025-03-22 17:06:39 +0530

    updated TechCommunity

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ChooseCard from "./Components/Why US/ChooseCard";
 import SuccessStory from "./Components/SuccessStory/SuccessStory";
 import Navbar from "./Components/Navbar/Navbar";
+import { TechCommunity } from "./Components/TechCommunity/TechCommunity";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
+      <TechCommunity/>
       <ChooseCard />
       <SuccessStory />
-    </div>
+    </>
   );
 };
 
